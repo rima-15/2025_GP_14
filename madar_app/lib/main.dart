d401f5633fd02c6f66be47565ab092b4b4e433c3
Mona <monaalnajjar2002@gmail.com>
2025-10-14 22:42:27 +0300

    Mona error messages

--- madar_app/lib/main.dart
+++ madar_app/lib/main.dart
@@ -4,30 +4,23 @@ import 'package:madar_app/screens/welcome_page.dart';
 import 'package:madar_app/widgets/MainLayout.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 1) Load .env BEFORE anything reads dotenv.get(...)
+  // 1) Load environment variables
   await dotenv.load(fileName: ".env");
 
-  // 2) Init Firebase
+  // 2) Initialize Firebase
   await Firebase.initializeApp();
 
-  // ✅ تحقق من حالة المستخدم و "Remember Me"
-  final prefs = await SharedPreferences.getInstance();
-  final remember = prefs.getBool('remember_me') ?? false;
   final user = FirebaseAuth.instance.currentUser;
 
-  // نحدد الصفحة اللي يبدأ منها التطبيق
   Widget startScreen;
-  if (remember && user != null) {
-    // المستخدم مفعّل "Remember Me" وموجود في Firebase
+  if (user != null) {
     startScreen = const MainLayout();
   } else {
-    // المستخدم مو مفعّل remember أو ما سجّل دخول
     startScreen = const WelcomeScreen();
   }
 

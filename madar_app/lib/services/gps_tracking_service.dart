bea189ba74c1753299e80f4a63c92d149c183427
Remas444 <remas207s@gmail.com>
2026-03-18 03:35:47 +0300

    commit

--- madar_app/lib/services/gps_tracking_service.dart
+++ madar_app/lib/services/gps_tracking_service.dart
@@ -6,7 +6,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 // ─────────────────────────────────────────────────────────────────────────────
